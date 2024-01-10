2b02cfa2231bbb90d5e0da46046753b515f69248
Vasil <vaskodzhakov@gmail.com>
2024-01-10 19:27:18 +0000

    Recharts add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,124 @@
-import Container from "@/components/ui/container";
+'use client'
 
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import Container from '@/components/ui/container'
+import { Minus, Plus } from 'lucide-react'
+import { useState } from 'react'
+import { Bar, BarChart, ResponsiveContainer } from 'recharts'
+
+const data = [
+  {
+    goal: 400
+  },
+  {
+    goal: 300
+  },
+  {
+    goal: 200
+  },
+  {
+    goal: 300
+  },
+  {
+    goal: 200
+  },
+  {
+    goal: 278
+  },
+  {
+    goal: 189
+  },
+  {
+    goal: 239
+  },
+  {
+    goal: 300
+  },
+  {
+    goal: 200
+  },
+  {
+    goal: 278
+  },
+  {
+    goal: 189
+  },
+  {
+    goal: 349
+  }
+]
 
 export default function Home() {
+  const [goal, setGoal] = useState(350)
+
+  function onClick(adjustment: number) {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)))
+  }
+
   return (
     <Container>
-      Content of page here
+      <div className="grid grid-cols-3 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Move Goal</CardTitle>
+            <CardDescription>Set your daily activity goal.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="p-4 pb-0">
+              <div className="flex items-center justify-center space-x-2">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-8 w-8 shrink-0 rounded-full"
+                  onClick={() => onClick(-10)}
+                  disabled={goal <= 200}>
+                  <Minus className="h-4 w-4" />
+                  <span className="sr-only">Decrease</span>
+                </Button>
+                <div className="flex-1 text-center">
+                  <div className="text-7xl font-bold tracking-tighter">{goal}</div>
+                  <div className="text-[0.70rem] uppercase text-muted-foreground">Calories/day</div>
+                </div>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="h-8 w-8 shrink-0 rounded-full"
+                  onClick={() => onClick(10)}
+                  disabled={goal >= 400}>
+                  <Plus className="h-4 w-4" />
+                  <span className="sr-only">Increase</span>
+                </Button>
+              </div>
+              <div className="mt-3 h-[120px]">
+                <ResponsiveContainer width="100%" height="100%">
+                  <BarChart data={data}>
+                    <Bar
+                      dataKey="goal"
+                      style={
+                        {
+                          fill: 'hsl(var(--foreground))',
+                          opacity: 0.9
+                        } as React.CSSProperties
+                      }
+                    />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+              <div className="mx-1.5">
+                <Button className="mt-5 w-full">Set Goal</Button>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </Container>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,13 @@
-"use client"
+'use client'
 
-import React from "react"
-import Container from "./ui/container"
-import Link from "next/link"
-import { Button } from "./ui/button"
-import { Menu, Moon, ShoppingCart, Sun } from "lucide-react"
-import ProfileButton from "./ui/ProfileButton"
-import { useTheme } from "next-themes"
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./ui/sheet"
+import React from 'react'
+import Container from './ui/container'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { Menu, Moon, ShoppingCart, Sun } from 'lucide-react'
+import ProfileButton from './ui/ProfileButton'
+import { useTheme } from 'next-themes'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 
 type Props = {}
 
@@ -23,26 +16,26 @@ const Header = () => {
 
   const routes = [
     {
-      href: "/",
-      label: "Products",
+      href: '/',
+      label: 'Products'
     },
     {
-      href: "/",
-      label: "Categories",
+      href: '/',
+      label: 'Categories'
     },
     {
-      href: "/",
-      label: "On Sale",
-    },
+      href: '/',
+      label: 'On Sale'
+    }
   ]
   return (
-    <header className="sm:flex sm:justify-between py-2 mb-5 border-b">
+    <header className="mb-5 border-b py-2 sm:flex sm:justify-between">
       <Container>
-        <div className="relative flex h-16 items-center justify-between w-full">
+        <div className="relative flex h-16  w-full items-center justify-between">
           <div className="flex items-center">
             <Sheet>
               <SheetTrigger>
-                <Menu className="h-6 md:hidden w-6" />
+                <Menu className="h-6 w-6 md:hidden" />
               </SheetTrigger>
               <SheetContent side="left" className="w-[300px] sm:w-[400px]">
                 <nav className="flex flex-col gap-4">
@@ -59,27 +52,18 @@ const Header = () => {
             </Link>
           </div>
 
-          <nav className="mx-6 items-center space-x-4 lg:space-x-6 hidden md:block">
+          <nav className="mx-6 hidden items-center space-x-4 md:block lg:space-x-6">
             {routes.map((route, i) => (
               // eslint-disable-next-line react/jsx-key
               <Button asChild variant="ghost">
-                <Link
-                  key={i}
-                  href={route.href}
-                  className="text-sm font-medium transition-colors"
-                >
+                <Link key={i} href={route.href} className="text-sm font-medium transition-colors">
                   {route.label}
                 </Link>
               </Button>
             ))}
           </nav>
           <div className="flex items-center">
-            <Button
-              variant="ghost"
-              size="icon"
-              className="mr-2"
-              aria-label="Shopping Cart"
-            >
+            <Button variant="ghost" size="icon" className="mr-2" aria-label="Shopping Cart">
               <ShoppingCart className="h-6 w-6" />
               <span className="sr-only">Shopping Cart</span>
             </Button>
@@ -88,8 +72,7 @@ const Header = () => {
               size="icon"
               aria-label="Toggle Theme"
               className="mr-6"
-              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-            >
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
               <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle Theme</span>
