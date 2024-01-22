4b7ba53b60e7610bd2e41e18af71fa000e855005
Vasil1001 <vaskodzhakov@gmail.com>
2024-01-22 14:38:03 +0000

    test

--- components/Header.tsx
+++ components/Header.tsx
@@ -71,7 +71,7 @@ const Header = () => {
               onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
               <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle Theme</span>
+              <span className="sr-only">Togglea Theme</span>
             </Button>
             <ProfileButton />
           </div>
